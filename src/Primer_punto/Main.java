4f9c73a22026ea09883f9cb62256246d2a4009b0
JucajudiISIS <jc.rodriguezp12@uniandes.edu.co>
2025-09-02 19:38:20 -0500

    Creacion de clases y mains

---
+++ src/Primer_punto/Main.java
@@ -0,0 +1,9 @@
+package Primer_punto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/Segundo_punto/Main.java
@@ -0,0 +1,10 @@
+package Segundo_punto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
